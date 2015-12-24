bc7833bc193b97422be43e7ef1eef704a17802d0
Jagaa <jagaa1103@gmail.com>
2015-12-24 18:35:48 +0900

    init

---
+++ iBeaconAndPush/iBeaconAndPush/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeaconAndPush
+//
+//  Created by Enkhjargal Gansukh on 12/24/15.
+//  Copyright © 2015 Enkhjargal Gansukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
